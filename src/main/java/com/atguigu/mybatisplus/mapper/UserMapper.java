29b9a305b85170e0919af672e9ee51bdbebbb515
admin <admin@example.com>
2023-08-07 16:49:39 +0800

    【P10】测试

---
+++ src/main/java/com/atguigu/mybatisplus/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.atguigu.mybatisplus.mapper;
+
+import com.atguigu.mybatisplus.bean.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+public interface UserMapper extends BaseMapper<User> {
+
+}
--- src/test/java/com/atguigu/mybatisplus/MybatisplusApplicationTests.java
+++ src/test/java/com/atguigu/mybatisplus/MybatisplusApplicationTests.java
@@ -1,13 +1,27 @@
 package com.atguigu.mybatisplus;
 
+import com.atguigu.mybatisplus.bean.User;
+import com.atguigu.mybatisplus.mapper.UserMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class MybatisplusApplicationTests {
 
+    @Autowired
+    UserMapper userMapper;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void testSelectList() {
+        List<User> users = userMapper.selectList(null);
+        users.forEach(System.out::println);
     }
 
 }
