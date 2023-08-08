6a3e1ee8f5b0f00fb829180090966ef3ee0ac94b
admin <admin@example.com>
2023-08-08 11:21:40 +0800

    【P13】测试BaseMapper的新增功能

--- src/test/java/com/atguigu/mybatisplus/MyBatisPlusTest.java
+++ src/test/java/com/atguigu/mybatisplus/MyBatisPlusTest.java
@@ -9,21 +9,29 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.util.List;
 
 @SpringBootTest
-class MybatisplusApplicationTests {
+public class MyBatisPlusTest {
 
     @Autowired
     UserMapper userMapper;
 
     @Test
-    void contextLoads() {
-    }
-
-    @Test
-    void testSelectList() {
+    public void testSelectList() {
         // 通过条件构造器查询一个list集合，若没有条件，则可以设置null为参数
         List<User> users = userMapper.selectList(null);
         users.forEach(System.out::println);
     }
 
+    @Test
+    public void testInsert() {
+        // 实现新增用户信息
+        // INSERT INTO user ( id, name, age, email ) VALUES ( ?, ?, ?, ? )
+        User user = new User();
+        user.setName("张三");
+        user.setAge(34);
+        user.setEmail("zhangsan@mail.com");
+        int result = userMapper.insert(user);
+        System.out.println("result:" + result);
+        System.out.println("id:" + user.getId()); //在mybatisplus中，默认用的雪花算法生成的id
+    }
 
 }
