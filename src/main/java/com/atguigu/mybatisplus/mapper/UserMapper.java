faf383e7e14878e6697d799c1359e4ec8602b712
admin <admin@example.com>
2023-08-10 11:17:41 +0800

    【P17】测试自定义功能

--- src/main/java/com/atguigu/mybatisplus/mapper/UserMapper.java
+++ src/main/java/com/atguigu/mybatisplus/mapper/UserMapper.java
@@ -3,6 +3,15 @@ package com.atguigu.mybatisplus.mapper;
 import com.atguigu.mybatisplus.bean.User;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 
+import java.util.Map;
+
 public interface UserMapper extends BaseMapper<User> {
+
+    /**
+     * 根据用户id查询用户信息为map集合
+     * @param id
+     * @return
+     */
+    Map<String, Object> selectMapById(Long id);
 
 }
