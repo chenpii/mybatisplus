a8825f3e46e125779ce0b8c72767c47cc1140e73
admin <admin@example.com>
2023-08-10 20:27:36 +0800

    【P31】Wrapper 条件构造器，组装排序条件

--- src/test/java/com/atguigu/mybatisplus/MybatisPlusWrapperTest.java
+++ src/test/java/com/atguigu/mybatisplus/MybatisPlusWrapperTest.java
@@ -20,9 +20,20 @@ public class MybatisPlusWrapperTest {
         // 查询用户名包含a，年龄在20到30之间，并且邮箱不为Null的用户信息
         // SELECT uid AS id,user_name AS name,age,email,is_deleted FROM t_user WHERE is_deleted=0 AND (user_name LIKE ? AND age BETWEEN ? AND ? AND email IS NOT NULL)
         QueryWrapper<User> userQueryWrapper = new QueryWrapper<>();
-        userQueryWrapper.like("user_name", "a")
+        userQueryWrapper.like("user_name", "a") //数据库中的字段名
                 .between("age", 20, 30)
                 .isNotNull("email");
+        List<User> users = userMapper.selectList(userQueryWrapper);
+        users.forEach(System.out::println);
+    }
+
+    @Test
+    public void test02() {
+        // 查询用户信息，按照年龄的降序排序，若年龄相同，则按照id升序排序
+        // SELECT uid AS id,user_name AS name,age,email,is_deleted FROM t_user WHERE is_deleted=0 ORDER BY age DESC,uid ASC
+        QueryWrapper<User> userQueryWrapper = new QueryWrapper<>();
+        userQueryWrapper.orderByDesc("age")
+                .orderByAsc("uid");
         List<User> users = userMapper.selectList(userQueryWrapper);
         users.forEach(System.out::println);
     }
