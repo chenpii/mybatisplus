61bfa809c54627158b704c532527ae88f25cc0ee
admin <admin@example.com>
2023-08-10 15:34:13 +0800

    【P21】mybati-plus常用注解@TableName；全局配置表前缀

--- src/main/java/com/atguigu/mybatisplus/bean/User.java
+++ src/main/java/com/atguigu/mybatisplus/bean/User.java
@@ -1,5 +1,6 @@
 package com.atguigu.mybatisplus.bean;
 
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,6 +8,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+// @TableName("t_user")
 public class User {
 
     private Long id;
