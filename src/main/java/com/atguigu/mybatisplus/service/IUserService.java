8dea5a01b9ce4ee4d2cb425fa7a50195e65420de
admin <admin@example.com>
2023-08-10 15:04:21 +0800

    【P18】通用Service接口

---
+++ src/main/java/com/atguigu/mybatisplus/service/IUserService.java
@@ -0,0 +1,7 @@
+package com.atguigu.mybatisplus.service;
+
+import com.atguigu.mybatisplus.bean.User;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+public interface IUserService extends IService<User> {
+}
---
+++ src/main/java/com/atguigu/mybatisplus/service/impl/UserServiceImpl.java
@@ -0,0 +1,9 @@
+package com.atguigu.mybatisplus.service.impl;
+
+import com.atguigu.mybatisplus.bean.User;
+import com.atguigu.mybatisplus.mapper.UserMapper;
+import com.atguigu.mybatisplus.service.IUserService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+
+public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
+}
