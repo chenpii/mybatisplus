fd562846baf3c00d49160aad6d05fa01fe5ef47c
admin <admin@example.com>
2023-08-15 09:07:07 +0800

    【P53】测试多数据源-@DS可以标在类、方法上

--- src/main/java/com/atguigu/mybatisplus/service/impl/UserServiceImpl.java
+++ src/main/java/com/atguigu/mybatisplus/service/impl/UserServiceImpl.java
@@ -7,7 +7,8 @@ import com.baomidou.dynamic.datasource.annotation.DS;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
 
-@DS("master") //指定操作的数据源
+@DS("master") //指定操作的数据源。
+// 如果同张表要读写分离，@DS注解可以标注不同的方法上来区分操作的数据源
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 }
